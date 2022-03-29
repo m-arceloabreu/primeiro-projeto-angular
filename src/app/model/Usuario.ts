7090dbcb7047e40d351a63b381ce4a5b5d6d8885
Marcelo <marceloabreu1997@gmail.com>
2022-03-29 19:58:37 -0300

    Rotas

---
+++ src/app/model/Usuario.ts
@@ -0,0 +1,18 @@
+import { Postagem } from "./Postagem";
+
+export class Usuario{
+
+    public id: number;
+
+    public nome: string;
+    public foto: string;
+    public tipo: string;
+    
+    public usuario: string;
+    public senha: string;
+
+
+    public postagem: Postagem [];
+    
+    
+}
